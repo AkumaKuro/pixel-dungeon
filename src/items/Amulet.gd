7f1749cb2719d5677058d21ebcfa380b87a404a7
AkumaKuro <p.ferreiraherda@gmail.com>
2025-08-25 22:06:09 -0400

    rewrite amulet

--- src/items/Amulet.gd
+++ src/items/Amulet.gd
@@ -1,104 +1,68 @@
-/*
- * Pixel Dungeon
- * Copyright (C) 2012-2015 Oleg Dolya
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
-package com.watabou.pixeldungeon.items;
+class_name Amulet
+extends Item
 
-import java.io.IOException;
-import java.util.ArrayList;
+const AC_END: String = "END THE GAME";
 
-import com.watabou.noosa.Game;
-import com.watabou.pixeldungeon.Badges;
-import com.watabou.pixeldungeon.Dungeon;
-import com.watabou.pixeldungeon.Statistics;
-import com.watabou.pixeldungeon.actors.hero.Hero;
-import com.watabou.pixeldungeon.scenes.AmuletScene;
-import com.watabou.pixeldungeon.sprites.ItemSpriteSheet;
+func _init() -> void:
+	name = "Amulet of Yendor";
+	image = ItemSpriteSheet.AMULET;
 
-public class Amulet extends Item {
+	unique = true;
 
-	private static final String AC_END = "END THE GAME";
 
-	{
-		name = "Amulet of Yendor";
-		image = ItemSpriteSheet.AMULET;
+#@Override
+func actions(hero: Hero) -> PackedStringArray:
+	var actions: PackedStringArray = super.actions( hero );
+	actions.add( AC_END );
+	return actions;
 
-		unique = true;
-	}
 
-	@Override
-	public ArrayList<String> actions( Hero hero ) {
-		ArrayList<String> actions = super.actions( hero );
-		actions.add( AC_END );
-		return actions;
-	}
+#@Override
+func execute(hero: Hero, action: String) -> void:
+	if (action == AC_END):
 
-	@Override
-	public void execute( Hero hero, String action ) {
-		if (action == AC_END) {
+		showAmuletScene( false );
 
-			showAmuletScene( false );
+	else:
 
-		} else {
+		super.execute( hero, action );
 
-			super.execute( hero, action );
 
-		}
-	}
 
-	@Override
-	public boolean doPickUp( Hero hero ) {
-		if (super.doPickUp( hero )) {
 
-			if (!Statistics.amuletObtained) {
-				Statistics.amuletObtained = true;
-				Badges.validateVictory();
+#@Override
+func doPickUp(hero: Hero) -> bool:
+	if (super.doPickUp( hero )):
 
-				showAmuletScene( true );
-			}
+		if (!Statistics.amuletObtained):
+			Statistics.amuletObtained = true;
+			Badges.validateVictory();
 
-			return true;
-		} else {
-			return false;
-		}
-	}
+			showAmuletScene( true );
 
-	private void showAmuletScene( boolean showText ) {
-		try {
-			Dungeon.saveAll();
-			AmuletScene.noText = !showText;
-			Game.switchScene( AmuletScene.class );
-		} catch (IOException e) {
-		}
-	}
-
-	@Override
-	public boolean isIdentified() {
 		return true;
-	}
-
-	@Override
-	public boolean isUpgradable() {
+	else:
 		return false;
-	}
 
-	@Override
-	public String info() {
-		return
-			"The Amulet of Yendor is the most powerful known artifact of unknown origin. It is said that the amulet " +
-			"is able to fulfil any wish if its owner's will-power is strong enough to \"persuade\" it to do it.";
-	}
-}
+
+
+func showAmuletScene(showText: bool) -> void:
+	Dungeon.saveAll();
+	AmuletScene.noText = !showText;
+	Game.switchScene( AmuletScene.class );
+
+#@Override
+func isIdentified() -> bool:
+	return true;
+
+
+#@Override
+func isUpgradable() -> bool:
+	return false;
+
+
+#@Override
+func info() -> String:
+	return \
+		"The Amulet of Yendor is the most powerful known artifact of unknown origin. It is said that the amulet " + \
+		"is able to fulfil any wish if its owner's will-power is strong enough to \"persuade\" it to do it.";
